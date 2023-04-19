af0ebb1d1adf9b2c934c75f33e2b530a45944b50
long-name-let-people-remember-you <ropz7609@gmail.com>
2023-04-19 16:42:50 +0800

    fix: NOT rule

--- rule/not.go
+++ rule/not.go
@@ -37,7 +37,7 @@ func (n *Not) ShouldFindProcess() bool {
 
 func NewNot(payload string, adapter string, params []string) (*Not, error) {
 	payloads := strings.SplitN(payload, ":", 2)
-	if len(payload) != 2 {
+	if len(payloads) != 2 {
 		return nil, fmt.Errorf("invalid NOT rule payload: %s", payload)
 	}
 	r, err := ParseRule(C.RuleType(0).FormatString(payloads[0]), payloads[1], adapter, params)
